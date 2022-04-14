a066ef0b61faaf00af193809284d71b95828fc94
brian d'ostilio <bd250227@ncr.com>
2022-04-14 11:30:38 -0400

    further cleanup

--- go/e6e/cmd/main_test.go
+++ go/e6e/cmd/main_test.go
@@ -14,14 +14,11 @@ import (
 func TestRunMain(t *testing.T) {
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-	done := make(chan bool, 1)
 
 	go func() {
 		main()
 	}()
 
 	sig := <-sigs
-	fmt.Println()
 	fmt.Println(sig)
-	done <- true
 }
